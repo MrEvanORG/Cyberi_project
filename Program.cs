de0886d113f034fc771a7044217478c57b5b797a
MrEvan_ORG <89385061+MrEvanORG@users.noreply.github.com>
2025-05-30 02:50:28 +0330

    base of console

--- Program.cs
+++ Program.cs
@@ -1,13 +1,93 @@
 ﻿using AllClass;
 using System;
-using System.IO.Pipes;
 namespace ConsoleProject
 {
     class Program
     {
+        static void ShowHelp(string command) 
+        {
+            string text = "";
+            if (command == "help") 
+            {
+                text = """
+
+                    For more information on a specific command, type HELP command-name 
+
+                        | Dormitory    show all command related to dormitories          |
+                        | Block        show all command related to buildings'block      |
+                        | Person       ``    ``     ``    ``    `` persons              |
+                        | Item         ``    ``     ``    ``    `` items                |
+                        | Reporting    ``    ``     ``    ``    `` reporting            |
+                    example : help block
+                    """;
+            }
+            else
+            {
+                switch (command.Split()[1]) 
+                {
+                    default:
+                        text = "Sorry there is not such subject to show help !";
+                        break;
+                    case "dormitory":
+                       text = "show all command related to dormitories";
+                        break;
+                    case "block":
+                        text = "show all command related to blockd";
+                        break;
+                    case "person":
+                        text = "show all command related to persons";
+                        break;
+                    case "item":
+                        text  = "show all command related to items";
+                        break;
+                    case "reporting":
+                        text = "show all command related to reporting";
+                        break;
+                }
+            }
+
+            Console.WriteLine(text);
+
+        }
         public static void Main(string[] args)
         {
-            Console.Write("salam"); 
+            ShowHelp("help");
+            while (true)
+            {
+                Console.Write("$ main>");
+                string input = Console.ReadLine();
+                string command = input.Trim().ToLower();
+
+                if (command.Equals("exit", StringComparison.OrdinalIgnoreCase)) break;
+
+                switch (command.Split()[0])
+                {
+                    default:
+                        if (command == "") break;
+                        Console.WriteLine("Command not found\nuse 'help' to see commands");
+                        break;
+                    case "help":
+                        ShowHelp(command);
+                        break;
+                    case "dormitory":
+                        while (true)
+                        {
+                            Console.Write("$ main/dormitory>");
+                            string input1 = Console.ReadLine();
+                            string command1 = input1.Trim().ToLower();
+                            if (command1.Equals("exit", StringComparison.OrdinalIgnoreCase)) break;
+                            switch (command1.Split()[0])
+                            {
+                                default:
+                                    if (command1 == "") break;
+                                    Console.WriteLine("Command not found\nuse 'help dormitory' to see commands");
+                                    break;
+                                case "help":
+                                    ShowHelp("help dormitory");
+                                    break;
+                            }
+                }
+            }
         }
     }
 }
