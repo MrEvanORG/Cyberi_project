cec5a14d0841f44713e1a944432ed35c5591fb4f
MrEvan_ORG <89385061+MrEvanORG@users.noreply.github.com>
2025-05-30 02:59:06 +0330

    base coonsole modified

--- Program.cs
+++ Program.cs
@@ -1,57 +1,65 @@
 ﻿using AllClass;
 using System;
+
 namespace ConsoleProject
 {
     class Program
     {
-        static void ShowHelp(string command) 
+        static void ShowHelp(string command)
         {
             string text = "";
-            if (command == "help") 
+            var parts = command.Split();
+
+            if (parts.Length == 1)
             {
                 text = """
 
                     For more information on a specific command, type HELP command-name 
 
                         | Dormitory    show all command related to dormitories          |
-                        | Block        show all command related to buildings'block      |
-                        | Person       ``    ``     ``    ``    `` persons              |
-                        | Item         ``    ``     ``    ``    `` items                |
-                        | Reporting    ``    ``     ``    ``    `` reporting            |
+                        | Block        show all command related to buildings' block     |
+                        | Person       show all command related to persons              |
+                        | Item         show all command related to items                |
+                        | Reporting    show all command related to reporting            |
                     example : help block
                     """;
             }
-            else
+            else if (parts.Length == 2)
             {
-                switch (command.Split()[1]) 
+                switch (parts[1])
                 {
                     default:
-                        text = "Sorry there is not such subject to show help !";
+                        text = "Sorry there is not such subject to show help!";
                         break;
                     case "dormitory":
-                       text = "show all command related to dormitories";
+                        text = "show all command related to dormitories";
                         break;
                     case "block":
-                        text = "show all command related to blockd";
+                        text = "show all command related to blocks";
                         break;
                     case "person":
                         text = "show all command related to persons";
                         break;
                     case "item":
-                        text  = "show all command related to items";
+                        text = "show all command related to items";
                         break;
                     case "reporting":
                         text = "show all command related to reporting";
                         break;
                 }
             }
+            else
+            {
+                text = "Invalid help usage.";
+            }
 
             Console.WriteLine(text);
-
         }
+
         public static void Main(string[] args)
         {
             ShowHelp("help");
+
             while (true)
             {
                 Console.Write("$ main>");
@@ -66,26 +74,33 @@ namespace ConsoleProject
                         if (command == "") break;
                         Console.WriteLine("Command not found\nuse 'help' to see commands");
                         break;
+
                     case "help":
                         ShowHelp(command);
                         break;
+
                     case "dormitory":
                         while (true)
                         {
                             Console.Write("$ main/dormitory>");
                             string input1 = Console.ReadLine();
                             string command1 = input1.Trim().ToLower();
+
                             if (command1.Equals("exit", StringComparison.OrdinalIgnoreCase)) break;
+
                             switch (command1.Split()[0])
                             {
                                 default:
                                     if (command1 == "") break;
                                     Console.WriteLine("Command not found\nuse 'help dormitory' to see commands");
                                     break;
+
                                 case "help":
                                     ShowHelp("help dormitory");
                                     break;
                             }
+                        }
+                        break; // این break مربوط به case "dormitory" است
                 }
             }
         }
