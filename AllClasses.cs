3ea925110e2437cfd69b96e307770e169e9c3b1e
FarhadXD <mfarhadt117@gmail.com>
2025-05-30 01:11:07 +0330

    Base Classes

--- AllClasses.cs
+++ AllClasses.cs
@@ -1,7 +1,43 @@
+using System;
 namespace AllClass
+{
+    public class Person
     {
-        class Person
+        public string Name;
+        public string NationalCode;
+        public string PhoneNumber;
+        public string Address;
+    }
+
+
+
+    public class Dormintory { }
+
+
+
+    public class Block { }
+
+
+
+    public class Room { }
+
+
+
+    public class Item { }
+
+
+
+    public class Student : Person
     {
-        public int Id { get; set; }
+        public string StudentId;
     }
-    }
+
+
+
+    public class BlochManeger : Student { }
+
+
+    
+    public class DormintoryManeger : Person { }
+    
+}
